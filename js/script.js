172ceb589c02860e3da5c5c9e1c2c20049c88af9
Jennifer Thatcher <jenniferthatcher@Jennifers-MacBook-Air.local>
2021-04-12 15:33:48 +0200

    checked html elements and changed names of variables in JS to match up with html

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-const unorderedListElement = document.querySelector(".unordered-list"); 
-const guessButton = document.querySelector(".guess");
-const textInput = document.querySelector(".text");
-const emptyPara = document.querySelector(".empty-para");
-const remainingGuessElement = document.querySelector(".remaining-guess");
-const remainingGuessSpan = document.querySelector(".remaining-guess-span");
-const remainingMessages = document.querySelector(".remaining-messages");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 
-//using placeholders until the correct word is guessed//
-
+// Display our symbols as placeholders for the chosen word's letters
 const placeholder = function (word) {
-    const placeholderLetters = [];
-    for (const letter of word) {
-        console.log(letter);
-        placeholderLetters.push("●");
-    }
-    emptyPara.innerText = placeholderLetters.join("");
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
 };
 
 placeholder(word);
 
-guessButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    const guess = textInput.nodeValue;
-    console.log(guess);
-    textInput.value = "";
+guessLetterButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = letterInput.value;
+  console.log(guess);
+  letterInput.value = "";
 });
